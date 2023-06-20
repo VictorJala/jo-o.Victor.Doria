878bacee400b02155f32c1c6b46c04c3864b420f
VictorJala <victor_doria11@hotmail.com>
2023-06-20 20:41:48 -0300

    aula 6: Menu, score, Death_count

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -25,7 +25,13 @@ class ObstacleManager:
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break
+
+    def restart_obstacle(self):
+        self.obstacles = []
+
+
 
     def draw(self, screen):
         for obstacle in self.obstacles:
